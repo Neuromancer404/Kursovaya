b5e5c69f87fdecf5e3406abb5bd3075e5abdbe3e
Neuromancer404 <73753089+Neuromancer404@users.noreply.github.com>
2021-11-24 13:18:34 +0300

    renderer without errors but with fucking black canvas

--- Renderer.js
+++ Renderer.js
@@ -1,6 +1,9 @@
+import * as THREE from 'https://unpkg.com/three@0.126.1/build/three.module.js';
+import { OrbitControls } from 'https://unpkg.com/three@0.126.1/examples/jsm/controls/OrbitControls.js';
+
 function main (){
 
-    let canvasWidth = 400;
+    let canvasWidth = 500;
     let canvasHeith = 400;
 
     const canvas = document.getElementById("canvasGL");
@@ -8,10 +11,22 @@ function main (){
     canvas.height = canvasHeith;
     canvas.width = canvasWidth;
 
-    const renderer = new THREE.CanvasRenderer({canvas});
-    renderer.setClearColor("white", 1.0);
+    const renderer = new THREE.WebGLRenderer({canvas});
     renderer.setSize(canvasWidth, canvasHeith);
 
-    console.log("fuckingFuck");
+    const fov = 45;
+    const aspect = 2;  // the canvas default
+    const near = 0.1;
+    const far = 100;
+    const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+    camera.position.set(0, 10, 20);
+
+    const controls = new OrbitControls( camera, renderer.domElement );
+    controls.target.set(0, 5, 0);
+    controls.update();
+
+    const scene = new THREE.Scene();
+    scene.background = new THREE.Color('blue');
+
 }
 main();
