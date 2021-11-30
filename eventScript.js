75ef8e893ede916e3be364be1339e5d5692b6685
Neuromancer404 <73753089+Neuromancer404@users.noreply.github.com>
2021-11-30 17:38:42 +0300

    Add files via upload

---
+++ eventScript.js
@@ -0,0 +1,64 @@
+//import {gl} from '/Renderer.js';
+
+window.onload = function(){
+    if (!isWebAssSupporting){
+        console.log("web ass not supporting");
+        window.alert("К сожалению, webassembly не поддерживается вашим браузером. Cкоро мы исправим неработоспособность, а пока полный функционал недоступен.");
+    }
+    else{ 
+        console.log ("your browser supporting webassembly");
+
+    //здесь запуск открытия окна выбора файла
+    drawScene();
+}
+}
+
+function isWebAssSupporting(){
+    try {
+        if (typeof WebAssembly === "object"){
+            const module = new WebAssembly.Module(new Uint8Array([0x00, 0x61, 0x73, 0x6D, 0x01, 0x00, 0x00, 0x00]));
+            if (module instanceof WebAssembly.Module){
+                const moduleInstance = new WebAssembly.Instance(module);
+                return (moduleInstance instanceof WebAssembly.Instance);
+            }
+        }
+    }catch (err){}
+    return false;
+}
+
+function resize(canvas) {
+    let displayWidth  = canvas.clientWidth;
+    let displayHeight = canvas.clientHeight;
+
+    if (canvas.width  != displayWidth ||
+        canvas.height != displayHeight) 
+        {   
+        canvas.width  = displayWidth;
+        canvas.height = displayHeight;
+    }
+  }
+
+
+function drawScene() {
+    let canvas = document.querySelector("#canvasGL");
+    resize(canvas);
+
+    }
+
+    (function() {
+   
+        'use strict';
+       
+        $('.input-file').each(function() {
+          var $input = $(this),
+              $label = $input.next('.js-labelFile'),
+              labelVal = $label.html();
+           
+         $input.on('change', function(element) {
+            var fileName = '';
+            if (element.target.value) fileName = element.target.value.split('\\').pop();
+            fileName ? $label.addClass('has-file').find('.js-fileName').html(fileName) : $label.removeClass('has-file').html(labelVal);
+         });
+        });
+       
+      })();
