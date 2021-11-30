0bd787fd7011d15ad0beab6f2cefb30560c32ba3
Neuromancer404 <73753089+Neuromancer404@users.noreply.github.com>
2021-11-30 11:55:15 +0300

    copypaste

--- fileHandler.js
+++ fileHandler.js
@@ -1,8 +1,47 @@
+let readResult;
+
 function readFile(object) {
-    var file = object.files[0]
-    var reader = new FileReader()
+    var file = object.files[0];
+    var reader = new FileReader();
     reader.onload = function() {
-        console.log(reader.result);
-    }
-    reader.readAsText(file)
+        readResult = reader.result;
+    };
+    reader.readAsText(file);
   }
+
+  loader.load(
+	// resource URL
+	'models/monster.obj',
+	// called when resource is loaded
+	function ( object ) {
+
+		scene.add( object );
+
+	},
+	// called when loading is in progresses
+	function ( xhr ) {
+
+		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+
+	},
+	// called when loading has errors
+	function ( error ) {
+
+		console.log( 'An error happened' );
+
+	}
+);
+
+  ///////////start magic/////////////
+
+  const importObject = {
+    env: {
+    __memory_base: 0,
+    }
+   };
+
+WebAssembly.instantiateStreaming( fetch("objParser.wasm"), importObject).then(result => 
+{
+    const value = result.instance.exports._LineRunner(readResult);
+    console.log(value.toString());
+});
---
+++ parser.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include <vector>
+
+char _LineRunner(std::vector <char> data)
+{
+
+    //for (int i=0; i<data.size(); i++){
+    //std::cout << "data[i]\n";
+    //}
+
+    return 1100;
+}
